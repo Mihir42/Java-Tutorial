cddbc2dd81bfe5a219df885eef3d7ffb4d17730f
Coffee <mihir_mistry@outlook.com>
2025-08-01 21:59:36 +0100

    Added search algorithms binary search

---
+++ Search Algorithms/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] integers = {1,2,4,5,7,9,11};
+        BinarySearch search = new BinarySearch();
+        System.out.println(search.binarySearch(integers, 11));
+
+    }
+}
