938e862ff1c61ba430b65f6fc29a06eb124d8929
Coffee <mihir_mistry@outlook.com>
2025-09-01 19:05:00 +0100

    Updated main file in constructors

--- Constructors/src/Main.java
+++ Constructors/src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
+//      Implicit default constructor
+        Animal dog = new Animal();
+
+//      Parameterized constructor
+        Animal cat = new Animal("Cat");
+
+//      Overloaded constructor
+        Animal sheep = new Animal("Sheep");
+        Animal wolf = new Animal("Wolf", "Carnivour");
+        Animal squirrel = new Animal("Squirrel", "Herbivour", "Mammel");
 
     }
 }
